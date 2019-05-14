08effae6fed0cdd64f2ff6446947633078f7c6b6
jomolo03 <jmonleonlopez@gmail.com>
2019-05-14 19:23:26 +0200

    Programar Formularios Varios

---
+++ ProyectoIntegrado/FormPagoTarjeta.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace ProyectoIntegrado
+{
+    partial class FormPagoTarjeta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormPagoTarjeta));
+            this.pcbCarrito = new System.Windows.Forms.PictureBox();
+            this.btnMinimizar = new System.Windows.Forms.PictureBox();
+            this.btnAtras = new System.Windows.Forms.Button();
+            this.lblAtras = new System.Windows.Forms.Label();
+            this.btnCerrar = new System.Windows.Forms.PictureBox();
+            this.grbNombreApellido = new System.Windows.Forms.GroupBox();
+            this.grpBarraInferior = new System.Windows.Forms.GroupBox();
+            this.grbBarraSuperior = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dgvItems = new System.Windows.Forms.DataGridView();
+            this.lblTituloPago = new System.Windows.Forms.Label();
+            this.lblNombreTitular = new System.Windows.Forms.Label();
+            this.lblNumeroTarjeta = new System.Windows.Forms.Label();
+            this.lblFechaCaducidad = new System.Windows.Forms.Label();
+            this.txtNombreTitular = new System.Windows.Forms.TextBox();
+            this.txtNumeroTarjeta = new System.Windows.Forms.TextBox();
+            this.dtCaducidadTarjeta = new System.Windows.Forms.DateTimePicker();
+            this.btnPagar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbCarrito)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMinimizar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnCerrar)).BeginInit();
+            this.grbNombreApellido.SuspendLayout();
+            this.grbBarraSuperior.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvItems)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pcbCarrito
+            // 
+            this.pcbCarrito.Image = ((System.Drawing.Image)(resources.GetObject("pcbCarrito.Image")));
+            this.pcbCarrito.Location = new System.Drawing.Point(13, 48);
+            this.pcbCarrito.Name = "pcbCarrito";
+            this.pcbCarrito.Size = new System.Drawing.Size(62, 50);
+            this.pcbCarrito.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcbCarrito.TabIndex = 42;
+            this.pcbCarrito.TabStop = false;
+            // 
+            // btnMinimizar
+            // 
+            this.btnMinimizar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMinimizar.BackColor = System.Drawing.Color.White;
+            this.btnMinimizar.Image = ((System.Drawing.Image)(resources.GetObject("btnMinimizar.Image")));
+            this.btnMinimizar.Location = new System.Drawing.Point(759, 10);
+            this.btnMinimizar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnMinimizar.Name = "btnMinimizar";
+            this.btnMinimizar.Size = new System.Drawing.Size(17, 16);
+            this.btnMinimizar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnMinimizar.TabIndex = 32;
+            this.btnMinimizar.TabStop = false;
+            // 
+            // btnAtras
+            // 
+            this.btnAtras.BackColor = System.Drawing.Color.White;
+            this.btnAtras.Image = ((System.Drawing.Image)(resources.GetObject("btnAtras.Image")));
+            this.btnAtras.Location = new System.Drawing.Point(-1, 365);
+            this.btnAtras.Name = "btnAtras";
+            this.btnAtras.Size = new System.Drawing.Size(62, 38);
+            this.btnAtras.TabIndex = 41;
+            this.btnAtras.UseVisualStyleBackColor = false;
+            // 
+            // lblAtras
+            // 
+            this.lblAtras.AutoSize = true;
+            this.lblAtras.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAtras.Location = new System.Drawing.Point(71, 376);
+            this.lblAtras.Name = "lblAtras";
+            this.lblAtras.Size = new System.Drawing.Size(44, 16);
+            this.lblAtras.TabIndex = 40;
+            this.lblAtras.Text = "Atrás";
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCerrar.Image = ((System.Drawing.Image)(resources.GetObject("btnCerrar.Image")));
+            this.btnCerrar.Location = new System.Drawing.Point(780, 10);
+            this.btnCerrar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(17, 16);
+            this.btnCerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnCerrar.TabIndex = 33;
+            this.btnCerrar.TabStop = false;
+            // 
+            // grbNombreApellido
+            // 
+            this.grbNombreApellido.Controls.Add(this.dtCaducidadTarjeta);
+            this.grbNombreApellido.Controls.Add(this.txtNumeroTarjeta);
+            this.grbNombreApellido.Controls.Add(this.txtNombreTitular);
+            this.grbNombreApellido.Controls.Add(this.lblFechaCaducidad);
+            this.grbNombreApellido.Controls.Add(this.lblNumeroTarjeta);
+            this.grbNombreApellido.Controls.Add(this.lblNombreTitular);
+            this.grbNombreApellido.Location = new System.Drawing.Point(348, 148);
+            this.grbNombreApellido.Name = "grbNombreApellido";
+            this.grbNombreApellido.Size = new System.Drawing.Size(314, 160);
+            this.grbNombreApellido.TabIndex = 37;
+            this.grbNombreApellido.TabStop = false;
+            // 
+            // grpBarraInferior
+            // 
+            this.grpBarraInferior.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.grpBarraInferior.Location = new System.Drawing.Point(-1, 409);
+            this.grpBarraInferior.Name = "grpBarraInferior";
+            this.grpBarraInferior.Size = new System.Drawing.Size(802, 41);
+            this.grpBarraInferior.TabIndex = 34;
+            this.grpBarraInferior.TabStop = false;
+            // 
+            // grbBarraSuperior
+            // 
+            this.grbBarraSuperior.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.grbBarraSuperior.Controls.Add(this.btnCerrar);
+            this.grbBarraSuperior.Controls.Add(this.btnMinimizar);
+            this.grbBarraSuperior.Location = new System.Drawing.Point(-1, 1);
+            this.grbBarraSuperior.Name = "grbBarraSuperior";
+            this.grbBarraSuperior.Size = new System.Drawing.Size(802, 41);
+            this.grbBarraSuperior.TabIndex = 33;
+            this.grbBarraSuperior.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.groupBox1.Location = new System.Drawing.Point(167, 34);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(72, 387);
+            this.groupBox1.TabIndex = 35;
+            this.groupBox1.TabStop = false;
+            // 
+            // dgvItems
+            // 
+            this.dgvItems.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvItems.Location = new System.Drawing.Point(13, 104);
+            this.dgvItems.Name = "dgvItems";
+            this.dgvItems.Size = new System.Drawing.Size(148, 135);
+            this.dgvItems.TabIndex = 36;
+            // 
+            // lblTituloPago
+            // 
+            this.lblTituloPago.AutoSize = true;
+            this.lblTituloPago.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTituloPago.Location = new System.Drawing.Point(447, 82);
+            this.lblTituloPago.Name = "lblTituloPago";
+            this.lblTituloPago.Size = new System.Drawing.Size(59, 20);
+            this.lblTituloPago.TabIndex = 12;
+            this.lblTituloPago.Text = "PAGO";
+            // 
+            // lblNombreTitular
+            // 
+            this.lblNombreTitular.AutoSize = true;
+            this.lblNombreTitular.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombreTitular.Location = new System.Drawing.Point(6, 16);
+            this.lblNombreTitular.Name = "lblNombreTitular";
+            this.lblNombreTitular.Size = new System.Drawing.Size(115, 16);
+            this.lblNombreTitular.TabIndex = 1;
+            this.lblNombreTitular.Text = "Nombre Titular:";
+            // 
+            // lblNumeroTarjeta
+            // 
+            this.lblNumeroTarjeta.AutoSize = true;
+            this.lblNumeroTarjeta.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumeroTarjeta.Location = new System.Drawing.Point(6, 47);
+            this.lblNumeroTarjeta.Name = "lblNumeroTarjeta";
+            this.lblNumeroTarjeta.Size = new System.Drawing.Size(116, 16);
+            this.lblNumeroTarjeta.TabIndex = 2;
+            this.lblNumeroTarjeta.Text = "NúmeroTarjeta:";
+            // 
+            // lblFechaCaducidad
+            // 
+            this.lblFechaCaducidad.AutoSize = true;
+            this.lblFechaCaducidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFechaCaducidad.Location = new System.Drawing.Point(6, 81);
+            this.lblFechaCaducidad.Name = "lblFechaCaducidad";
+            this.lblFechaCaducidad.Size = new System.Drawing.Size(126, 16);
+            this.lblFechaCaducidad.TabIndex = 3;
+            this.lblFechaCaducidad.Text = "FechaCaducidad";
+            this.lblFechaCaducidad.Click += new System.EventHandler(this.lblHora_Click);
+            // 
+            // txtNombreTitular
+            // 
+            this.txtNombreTitular.Location = new System.Drawing.Point(152, 16);
+            this.txtNombreTitular.Name = "txtNombreTitular";
+            this.txtNombreTitular.Size = new System.Drawing.Size(100, 20);
+            this.txtNombreTitular.TabIndex = 10;
+            // 
+            // txtNumeroTarjeta
+            // 
+            this.txtNumeroTarjeta.Location = new System.Drawing.Point(152, 47);
+            this.txtNumeroTarjeta.Name = "txtNumeroTarjeta";
+            this.txtNumeroTarjeta.Size = new System.Drawing.Size(100, 20);
+            this.txtNumeroTarjeta.TabIndex = 11;
+            // 
+            // dtCaducidadTarjeta
+            // 
+            this.dtCaducidadTarjeta.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtCaducidadTarjeta.Location = new System.Drawing.Point(152, 81);
+            this.dtCaducidadTarjeta.Name = "dtCaducidadTarjeta";
+            this.dtCaducidadTarjeta.Size = new System.Drawing.Size(102, 20);
+            this.dtCaducidadTarjeta.TabIndex = 12;
+            // 
+            // btnPagar
+            // 
+            this.btnPagar.Location = new System.Drawing.Point(451, 365);
+            this.btnPagar.Name = "btnPagar";
+            this.btnPagar.Size = new System.Drawing.Size(75, 23);
+            this.btnPagar.TabIndex = 43;
+            this.btnPagar.Text = "Pagar";
+            this.btnPagar.UseVisualStyleBackColor = true;
+            // 
+            // FormPagoTarjeta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnPagar);
+            this.Controls.Add(this.lblTituloPago);
+            this.Controls.Add(this.pcbCarrito);
+            this.Controls.Add(this.btnAtras);
+            this.Controls.Add(this.lblAtras);
+            this.Controls.Add(this.grbNombreApellido);
+            this.Controls.Add(this.grpBarraInferior);
+            this.Controls.Add(this.grbBarraSuperior);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dgvItems);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormPagoTarjeta";
+            this.Text = "FormPagoTarjeta";
+            ((System.ComponentModel.ISupportInitialize)(this.pcbCarrito)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMinimizar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnCerrar)).EndInit();
+            this.grbNombreApellido.ResumeLayout(false);
+            this.grbNombreApellido.PerformLayout();
+            this.grbBarraSuperior.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvItems)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pcbCarrito;
+        private System.Windows.Forms.PictureBox btnMinimizar;
+        private System.Windows.Forms.Button btnAtras;
+        private System.Windows.Forms.Label lblAtras;
+        private System.Windows.Forms.PictureBox btnCerrar;
+        private System.Windows.Forms.GroupBox grbNombreApellido;
+        private System.Windows.Forms.GroupBox grpBarraInferior;
+        private System.Windows.Forms.GroupBox grbBarraSuperior;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dgvItems;
+        private System.Windows.Forms.Label lblTituloPago;
+        private System.Windows.Forms.TextBox txtNumeroTarjeta;
+        private System.Windows.Forms.TextBox txtNombreTitular;
+        private System.Windows.Forms.Label lblFechaCaducidad;
+        private System.Windows.Forms.Label lblNumeroTarjeta;
+        private System.Windows.Forms.Label lblNombreTitular;
+        private System.Windows.Forms.DateTimePicker dtCaducidadTarjeta;
+        private System.Windows.Forms.Button btnPagar;
+    }
+}
---
+++ ProyectoIntegrado/FormPagoTarjeta.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoIntegrado
+{
+    public partial class FormPagoTarjeta : Form
+    {
+        public FormPagoTarjeta()
+        {
+            InitializeComponent();
+        }
+
+        private void lblHora_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
