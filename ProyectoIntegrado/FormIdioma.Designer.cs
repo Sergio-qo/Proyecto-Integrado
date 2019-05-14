39690805f8f237acaf9613d56ba1c176ae027806
jomolo03 <jmonleonlopez@gmail.com>
2019-05-14 18:44:55 +0200

    Añadir algunos botones de Cerrar/Minimizar y eliminar la carpeta AEV6 de Xavi

--- ProyectoIntegrado/FormIdioma.Designer.cs
+++ ProyectoIntegrado/FormIdioma.Designer.cs
@@ -38,11 +38,18 @@
             this.btnAtras = new System.Windows.Forms.Button();
             this.lblAtras = new System.Windows.Forms.Label();
             this.btnAceptarIdioma = new System.Windows.Forms.Button();
+            this.btnCerrar = new System.Windows.Forms.PictureBox();
+            this.btnMinimizar = new System.Windows.Forms.PictureBox();
+            this.grbBarraSuperior.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnCerrar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMinimizar)).BeginInit();
             this.SuspendLayout();
             // 
             // grbBarraSuperior
             // 
             this.grbBarraSuperior.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.grbBarraSuperior.Controls.Add(this.btnMinimizar);
+            this.grbBarraSuperior.Controls.Add(this.btnCerrar);
             this.grbBarraSuperior.Location = new System.Drawing.Point(-1, 0);
             this.grbBarraSuperior.Name = "grbBarraSuperior";
             this.grbBarraSuperior.Size = new System.Drawing.Size(802, 41);
@@ -133,6 +140,33 @@
             this.btnAceptarIdioma.UseVisualStyleBackColor = true;
             this.btnAceptarIdioma.Click += new System.EventHandler(this.btnAceptarIdioma_Click);
             // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCerrar.Image = ((System.Drawing.Image)(resources.GetObject("btnCerrar.Image")));
+            this.btnCerrar.Location = new System.Drawing.Point(780, 11);
+            this.btnCerrar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(17, 16);
+            this.btnCerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnCerrar.TabIndex = 22;
+            this.btnCerrar.TabStop = false;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // btnMinimizar
+            // 
+            this.btnMinimizar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMinimizar.BackColor = System.Drawing.Color.White;
+            this.btnMinimizar.Image = ((System.Drawing.Image)(resources.GetObject("btnMinimizar.Image")));
+            this.btnMinimizar.Location = new System.Drawing.Point(759, 11);
+            this.btnMinimizar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnMinimizar.Name = "btnMinimizar";
+            this.btnMinimizar.Size = new System.Drawing.Size(17, 16);
+            this.btnMinimizar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnMinimizar.TabIndex = 23;
+            this.btnMinimizar.TabStop = false;
+            this.btnMinimizar.Click += new System.EventHandler(this.btnMinimizar_Click);
+            // 
             // FormIdioma
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -152,6 +186,9 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FormIdioma";
             this.Load += new System.EventHandler(this.FormIdioma_Load);
+            this.grbBarraSuperior.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.btnCerrar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMinimizar)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -168,5 +205,7 @@
         private System.Windows.Forms.Button btnAtras;
         private System.Windows.Forms.Label lblAtras;
         private System.Windows.Forms.Button btnAceptarIdioma;
+        private System.Windows.Forms.PictureBox btnCerrar;
+        private System.Windows.Forms.PictureBox btnMinimizar;
     }
 }
