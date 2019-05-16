1b39512d569eb07e38cb69eb69d07403ec3eef7f
ORDENADOR-XAVI\Xavi <xavcanfab@gmail.com>
2019-05-16 19:53:35 +0200

    Modifico codigo FormPago y FormMesaReservada

--- ProyectoIntegrado/FormMesaReservada.cs
+++ ProyectoIntegrado/FormMesaReservada.cs
@@ -17,6 +17,9 @@ namespace ProyectoIntegrado
             InitializeComponent();
         }
 
-
+        private void BtnSiguienteDisponible_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
