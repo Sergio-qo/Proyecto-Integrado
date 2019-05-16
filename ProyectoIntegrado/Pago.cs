29c5d7543055252dfccc4108635b2338b69ebb8b
ALUMNADO\toolgo <tomytrt@gmail.com>
2019-05-16 18:45:42 +0200

    Subo clase Pago

---
+++ ProyectoIntegrado/Pago.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace ProyectoIntegrado
+{
+    class Pago
+    {
+        public Pago()
+        {
+
+        }
+     public double getPrecioTotal(Pedidos pedido)
+        {
+            String consulta = "SELECT precio FROM ArticulosPedido where id=$id";
+            ConexionBBDD conexion = new ConexionBBDD();
+
+            MySqlCommand comando = new MySqlCommand(consulta, conexion.Conexion);
+            comando.Parameters.AddWithValue("id", pedido.PrecioPedido);
+            MySqlDataReader reader = comando.ExecuteReader();
+            double preciotot = reader.GetDouble(0);
+           
+            return preciotot;
+        }
+
+    }
+}
